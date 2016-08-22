5dbc102e11824c8b441c2fd79fa16a2951e575b5
chapeter <chapeter@cisco.com>
2016-08-22 15:29:10 -0700

    get answer

--- 1/c1.py
+++ 1/c1.py
@@ -1,8 +1,6 @@
 import requests
 import json
-from flask import Flask
 
-app = Flask(__name__)
 token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNoYXBldGVyIiwicm9sZSI6InVzZXIiLCJpYXQiOjE0NzE4OTcxNDMsImV4cCI6MTQ3MzEwNjc0M30.nPef98Cj_ibO2dzbQ3--pp0LR-EasoPDjlS5AesV6Gs"
 
 
@@ -23,7 +21,6 @@ def submitAnswer(answer, token):
 
     return response
 
-@app.route("/")
 def getAnswer():
     url = "https://1faoejtuza.execute-api.us-east-1.amazonaws.com/prod/gsx-challenge"
 
@@ -31,13 +28,12 @@ def getAnswer():
         'content-type': "application/json"
     }
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers).json()
+    answer = response["answer"]
+    submitAnswer(answer, token)
 
-    return response.text
+    return
 
 
+getAnswer()
 
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
